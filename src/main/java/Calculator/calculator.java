c7ff4fccd4f5b06dbda13fe6e26fdfc0d9e75646
Naz <monazzah.dar@outlook.com>
2021-08-24 16:58:04 +0100

    MD:commit ex3

--- src/main/java/Calculator/calculator.java
+++ src/main/java/Calculator/calculator.java
@@ -1,10 +1,31 @@
 package Calculator;
 
 public class calculator {
+	
+	public static void main (String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(addition(10d, 4d));
+		System.out.println(subtract(20d, 10d));
+		System.out.println(multiply(5d, 5d));
+		divide(20d, 2d);
 
 	}
+	public static double addition(double num1, double num2) {
+		return num1 + num2;
+	}
+	public static double subtract(double num1, double num2) {
+		return num1 - num2;
+	}
+	public static double multiply(double num1, double num2) {
+		return num1 * num2;
+	}
 
+	public static void divide(double num1, double num2) {
+		if (num2 == 0) {
+		System.out.println("not divisible");
+		}else {
+			System.out.println(num1 / num2);
+		}
+	}
 }
+
