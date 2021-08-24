fdb812d3892681fe4c4edfd2705618c5a90dcbd5
Naz <monazzah.dar@outlook.com>
2021-08-24 14:57:30 +0100

    MD:commit ex1

--- src/main/java/HelloWorld/HelloWorld.java
+++ src/main/java/HelloWorld/HelloWorld.java
@@ -2,4 +2,20 @@ package HelloWorld;
 
 public class HelloWorld {
 
-}
+		public static void main(String[] args) {
+
+			System.out.println("Hello World!");
+			String message = "Hello World!";
+			System.out.println(message);
+			printMessage();
+			printMessage();
+			printMessage();
+			printMessage();
+		}
+
+		public static void printMessage() {
+			String message = "Hello Naz";
+			System.out.println(message);
+		}
+
+	}
