efc68327d437e11795ed040caf81400935bb31b1
Naz <monazzah.dar@outlook.com>
2021-08-24 15:26:50 +0100

    MD:commit ex2

--- src/main/java/HelloWorld/HelloWorld.java
+++ src/main/java/HelloWorld/HelloWorld.java
@@ -4,13 +4,10 @@ public class HelloWorld {
 
 		public static void main(String[] args) {
 
-			System.out.println("Hello World!");
 			String message = "Hello World!";
 			System.out.println(message);
 			printMessage();
-			printMessage();
-			printMessage();
-			printMessage();
+			
 		}
 
 		public static void printMessage() {
